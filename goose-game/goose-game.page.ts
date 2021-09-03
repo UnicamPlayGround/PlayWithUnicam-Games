66247ff9a6adf31d8bbd1188c3a9879479c73ec7
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2021-09-03 12:26:53 +0200

    migliorie front-end + fix funzionamento
    (funziona tutto)

--- goose-game/goose-game.page.ts
+++ goose-game/goose-game.page.ts
@@ -28,8 +28,10 @@ export class GooseGamePage implements OnInit {
 
   // true se è il mio turno, false altrimenti
   myTurn = false;
+
+  abilitaDado = false;
   cells = [
-    { title: 'START' },
+    { title: '0' },
     { title: '1', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
     { title: '2', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
     { title: '3', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
@@ -45,6 +47,30 @@ export class GooseGamePage implements OnInit {
     { title: '13', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
     { title: '14', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
     { title: '15', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '16', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '17', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '18', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '19', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '20', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '21', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '22', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '23', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '24', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '25', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '26', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '27', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '28', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '29', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '30', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '31', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '32', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '33', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '34', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '35', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '36', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '37', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '38', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } },
+    // { title: '39', question: { q: 'Che ora è?', a1: 'le 3', a2: 'le 4', a3: 'le 5' } }
   ];
 
   info_partita = { codice: null, codice_lobby: null, giocatore_corrente: null, id_gioco: null, info: null, vincitore: null };
@@ -69,7 +95,157 @@ export class GooseGamePage implements OnInit {
     this.timerPing = timerService.getTimer(() => { this.ping() }, 4000);
   }
 
-  async ngOnInit() { }
+  async ngOnInit() {
+    console.log('this.lobbyPlayers', this.lobbyPlayers);
+    console.log('this.gamePlayers', this.gamePlayers);
+    this.createGameBoard();
+    //this.createPlayersGoose();
+  }
+
+  /**
+   * Costruisce le pedine dei vari giocatori che partecipano
+   * alla partita. 
+   */
+  createPlayersGoose() {
+    console.log("sono dentro createGoose");
+    this.gamePlayers.forEach(player => {
+      console.log('player', player);
+      const goose = document.createElement("div");
+      goose.id = player.goose;
+      goose.title = player.username;
+      goose.classList.add("goose");
+      goose.classList.add(player.goose);
+      goose.appendChild(this.getGooseName(player.username));
+      goose.appendChild(this.getGooseImg(player.goose));
+      document.getElementById("c0").appendChild(goose);
+      console.log(goose);
+    });
+  }
+
+  /**
+   * Crea e ritorna un elemento <p> il cui testo è dato dal
+   * valore di 'username'.
+   * @param username Il testo da scrivere dentro il <p>.
+   * @returns L'elemento <p> creato.
+   */
+  getGooseName(username) {
+    const name = document.createElement("p");
+    name.textContent = username;
+    console.log('name', name);
+    return name;
+  }
+
+  /**
+   * Crea e ritorna un elemento <img> la cui 'src' è data dal
+   * file il cui basename è il valore di 'goose'.
+   * 
+   * @param goose Il basename dell'immagine da aprire.
+   * @returns L'elemento <img> creato.
+   */
+  getGooseImg(goose) {
+    const img = document.createElement("img");
+    img.src = "../../../assets/game-assets/" + goose + ".png";
+    console.log('img', img);
+    return img;
+  }
+
+  /**
+   * Costruisce il tabellone di gioco in base alle caselle contenute in 'this.cells'.
+   */
+  createGameBoard() {
+    console.log("sono dentro createGameBoard");
+    var currentRowNumber = 0;
+    var direction = true;
+
+    this.cells.forEach(cell => {
+      const newCell = this.createGameCell(cell.title);
+      const currentRow = document.getElementById("row" + currentRowNumber);
+
+      if (currentRow.childNodes.length < 8) {
+        if (currentRowNumber % 2 == 0) this.appendChildByDirection(currentRow, newCell, direction);
+        else {
+          this.fillRowWithEmptyCells(currentRow, direction);
+          this.createNewRow(++currentRowNumber).appendChild(newCell);
+        }
+      } else {
+        direction = !direction;
+        this.createNewRow(++currentRowNumber).appendChild(newCell);
+      }
+    });
+
+    if (document.getElementById("row" + currentRowNumber).childNodes.length < 8) {
+      this.fillRowWithEmptyCells(document.getElementById("row" + currentRowNumber), direction);
+    }
+  }
+
+  /**
+   * Crea una nuova riga del tabellone e la ritorna.
+   * 
+   * @param currentRowNumber Il numero della nuova riga da creare.
+   * @returns La nuova riga creata.
+   */
+  createNewRow(currentRowNumber) {
+    const newRow = document.createElement("tr");
+    newRow.id = "row" + currentRowNumber;
+    newRow.classList.add("row");
+    document.getElementById("table").appendChild(newRow);
+    return newRow;
+  }
+
+  /**
+   * Crea una nuova casella di gioco con all'interno una label contentente il numero della casella.
+   * 
+   * @param cellNumber Il numero della casella da creare.
+   * @returns La nuova casella creata.
+   */
+  createGameCell(cellNumber) {
+    const newCell = document.createElement("td");
+    newCell.id = "c" + cellNumber;
+    newCell.classList.add("game-cell");
+    const label = document.createElement("ion-label");
+    label.textContent = cellNumber;
+    newCell.appendChild(label);
+    return newCell;
+  }
+
+  /**
+   * Crea una nuova casella vuota (invisibile).
+   * 
+   * @returns La casella vuota creata.
+   */
+  createEmptyCell() {
+    const emptyCell = document.createElement("td");
+    emptyCell.classList.add("empty-cell");
+    return emptyCell;
+  }
+
+  /**
+   * Aggiunge il nodo 'child' come figlio del nodo 'parent' a seconda del valore
+   * di 'direction': se è true aggiunge in coda, altrimenti aggiunge all'inizio.
+   * 
+   * @param parent Il container dentro cui aggiungere il nuovo nodo.
+   * @param child Il nuovo nodo da aggiungere dentro il nodo container.
+   * @param direction Determina dove aggiungere il nuovo nodo.
+   */
+  appendChildByDirection(parent, child, direction) {
+    if (direction) parent.appendChild(child);
+    else parent.insertBefore(child, parent.firstChild);
+  }
+
+  /**
+   * Aggiunge alla riga passata in input tante caselle vuote quante necessarie
+   * affinché la riga contenga un totale di 8 caselle.
+   * Se 'direction' è true le aggiunge in coda, altrimenti le aggiunge all'inizio.
+   * 
+   * @param currentRow La riga a cui aggiungere caselle vuote.
+   * @param direction Il verso di aggiunta delle caselle vuote.
+   */
+  fillRowWithEmptyCells(currentRow, direction) {
+    const nToAdd = 8 - currentRow.childNodes.length;
+
+    for (let i = 0; i < nToAdd; i++)
+      this.appendChildByDirection(currentRow, this.createEmptyCell(), direction);
+  }
 
   /**
    * Recupera i partecipanti della lobby.
@@ -106,6 +282,7 @@ export class GooseGamePage implements OnInit {
       this.gamePlayers.push(tmp);
       counter++;
     });
+    this.createPlayersGoose();
     this.getInfoPartita();
   }
 
@@ -153,13 +330,6 @@ export class GooseGamePage implements OnInit {
 
             if (lancio != 0)
               this.muoviPedina(player.goose, this.getPosizionePedina(player.goose), lancio);
-
-            // var aggiornamento = setTimeout(() => {
-            //   console.log('nel timeout');
-            //   if (this.info_partita.giocatore_corrente == this.gamePlayers[this.localPlayerIndex].username && !this.myTurn)
-            //      this.iniziaTurno();
-            //   clearTimeout(aggiornamento);
-            // }, 1200 * lancio);
           }
         });
         mosseAggiornate = [];
@@ -192,6 +362,7 @@ export class GooseGamePage implements OnInit {
   iniziaTurno() {
     this.alertCreator.createInfoAlert('Tocca a te!', 'È il tuo turno, tira il dado per procedere!');
     this.myTurn = true;
+    this.abilitaDado = !this.abilitaDado;
   }
 
   private async inviaDatiPartita(info, fineTurno) {
@@ -263,7 +434,7 @@ export class GooseGamePage implements OnInit {
 
   private calcolaClassifica() {
     var classifica = [];
-    var numeroCaselle = 15;
+    var numeroCaselle = this.cells.length - 1;
     this.gamePlayers.forEach(player => {
       var posizione = 0;
       player.info.forEach(lancio => {
@@ -316,7 +487,7 @@ export class GooseGamePage implements OnInit {
   }
 
   private controllaFinePartita(posizione, lancio, goose, intervalloMovimentoPedina) {
-    if (posizione == 14 && lancio == 1) {
+    if (posizione == (this.cells.length - 2) && lancio == 1) {
       this.inviaDatiPartita(this.gamePlayers[this.localPlayerIndex].info, true);
 
       var button = [{ text: 'Vai alla classifica', handler: () => { this.mostraClassifica(); } }];
@@ -334,7 +505,6 @@ export class GooseGamePage implements OnInit {
   muoviPedina(goose, posizione, lancio) {
     const intervalloMovimentoPedina = setInterval(() => {
       if (lancio == 0) {
-        console.log("il lancio è = 0");
         clearInterval(intervalloMovimentoPedina);
 
         if (goose == this.gamePlayers[this.localPlayerIndex].goose)
@@ -344,8 +514,7 @@ export class GooseGamePage implements OnInit {
           this.iniziaTurno();
 
       } else {
-        console.log("intervallo");
-        //TODO da rigaurdare
+
         if (this.controllaDirezionePedina(posizione, lancio)) {
           this.controllaFinePartita(posizione, lancio, goose, intervalloMovimentoPedina);
           document.getElementById('c' + (++posizione)).appendChild(document.getElementById(goose));
@@ -355,8 +524,6 @@ export class GooseGamePage implements OnInit {
             clearInterval(intervalloMovimentoPedina);
             document.getElementById('c' + (--posizione)).appendChild(document.getElementById(goose));
             this.tornaIndietro(goose, posizione, --lancio);
-            if (goose == this.gamePlayers[this.localPlayerIndex].goose)
-              this.presentaDomanda();
 
           } else if (lancio == 1) {
             clearInterval(intervalloMovimentoPedina);
@@ -376,7 +543,7 @@ export class GooseGamePage implements OnInit {
    * @returns true se la Pedina può continuare ad andare avanti, false altrimenti.
    */
   controllaDirezionePedina(posizione, lancio) {
-    if (posizione == 15 && lancio >= 1)
+    if (posizione == (this.cells.length - 1) && lancio >= 1)
       return false;
     else
       return true;
@@ -402,6 +569,7 @@ export class GooseGamePage implements OnInit {
   }
 
   lanciaDado() {
+    this.abilitaDado = !this.abilitaDado;
     var lancio = Math.floor(Math.random() * 6) + 1;
 
     this.gamePlayers[this.localPlayerIndex].info.push(lancio);
