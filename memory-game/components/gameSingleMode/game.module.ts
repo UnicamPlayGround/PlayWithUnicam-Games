d5ba2eed6ac3f9c471aac460077d2c889ae4cb49
C1PO <74489318+C1PO@users.noreply.github.com>
2021-12-13 17:38:44 +0100

    Revert "refactor cartelle"
    This reverts commit 7609fa1e4ae5cb86f2d277cdb7dfc0ff6c8bc1b7.

--- memory-game/components/gameSingleMode/game.module.ts
+++ memory-game/components/gameSingleMode/game.module.ts
@@ -7,10 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { GamePageRoutingModule } from './game-routing.module';
 
 import { GamePage } from './game.page';
-import { MemoryCardComponent } from '../components/memory-card/memory-card.component';
+import { MemoryCardComponent } from '../memory-card/memory-card.component';
 import { CellQuestionPage } from 'src/app/mgp_games/goose-game/modal/cell-question/cell-question.page';
-import { CardQuestionPage } from '../modal-page/card-question/card-question.page';
-import { CardQuestionPageModule } from '../modal-page/card-question/card-question.module';
+import { CardQuestionPage } from '../../modal-page/card-question/card-question.page';
+import { CardQuestionPageModule } from '../../modal-page/card-question/card-question.module';
 
 @NgModule({
   imports: [
